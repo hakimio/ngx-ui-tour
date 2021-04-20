8f666d7f1eaba2bf3660c7e7c08640ba3f9da050
Tomas Rimkus <sandshrew@gmail.com>
2021-04-20 11:56:22 +0200

    Block interaction with base web page when tour backdrop is active.

--- projects/ngx-ui-tour-core/src/lib/tour-backdrop.service.ts
+++ projects/ngx-ui-tour-core/src/lib/tour-backdrop.service.ts
@@ -7,6 +7,7 @@ import {ScrollingUtil} from './scrolling-util';
 @Injectable()
 export class TourBackdropService {
   private renderer: Renderer2;
+  private backdropBaseElement: HTMLElement;
   private backdropElement: HTMLElement;
   private targetHtmlElement: HTMLElement;
   windowResizeSubscription$: Subscription;
@@ -19,18 +20,41 @@ export class TourBackdropService {
     const boundingRect = targetElement.nativeElement.getBoundingClientRect();
 
     if (!this.backdropElement) {
-      this.backdropElement = this.renderer.createElement('div');
-      this.renderer.addClass(this.backdropElement, 'ngx-ui-tour_backdrop');
-      this.renderer.appendChild(document.body, this.backdropElement);
-
+      this.createBackdrop();
       this.subscribeToWindowResizeEvent();
     }
 
     this.targetHtmlElement = targetElement.nativeElement;
-    this.setStyles(boundingRect);
+    this.setBackdropElStyles(boundingRect);
   }
 
-  subscribeToWindowResizeEvent() {
+  private createBackdrop() {
+    this.createBackdropBaseElement();
+    this.createBackdropElement();
+  }
+
+  private createBackdropBaseElement() {
+    const baseElStyles: Partial<CSSStyleDeclaration> = {
+      position: 'fixed',
+      height: '100%',
+      width: '100%',
+      top: '0',
+      left: '0',
+      zIndex: '100'
+    };
+
+    this.backdropBaseElement = this.renderer.createElement('div');
+    this.applyStyles(baseElStyles, this.backdropBaseElement);
+    this.renderer.appendChild(document.body, this.backdropBaseElement);
+  }
+
+  private createBackdropElement() {
+    this.backdropElement = this.renderer.createElement('div');
+    this.renderer.addClass(this.backdropElement, 'ngx-ui-tour_backdrop');
+    this.renderer.appendChild(document.body, this.backdropElement);
+  }
+
+  private subscribeToWindowResizeEvent() {
     const resizeObservable$ = fromEvent(window, 'resize');
     this.windowResizeSubscription$ = resizeObservable$
       .pipe(
@@ -39,7 +63,7 @@ export class TourBackdropService {
       .subscribe(
         () => {
           const boundingRect = this.targetHtmlElement.getBoundingClientRect();
-          this.setStyles(boundingRect);
+          this.setBackdropElStyles(boundingRect);
           ScrollingUtil.ensureVisible(this.targetHtmlElement);
         }
       );
@@ -47,27 +71,38 @@ export class TourBackdropService {
 
   public close() {
     if (this.backdropElement) {
-      this.renderer.removeChild(document.body, this.backdropElement);
+      this.removeBackdropElement();
       this.windowResizeSubscription$.unsubscribe();
-      this.backdropElement = null;
     }
   }
 
-  private setStyles(boundingRect: DOMRect) {
+  private removeBackdropElement() {
+    this.renderer.removeChild(document.body, this.backdropBaseElement);
+    this.renderer.removeChild(document.body, this.backdropElement);
+    this.backdropBaseElement = null;
+    this.backdropElement = null;
+  }
+
+  private setBackdropElStyles(boundingRect: DOMRect) {
     const scrollX = window.scrollX ?? window.pageXOffset,
       scrollY = window.scrollY ?? window.pageYOffset,
-      styles = {
+      styles: Partial<CSSStyleDeclaration> = {
         position: 'absolute',
         width: `${boundingRect.width}px`,
         height: `${boundingRect.height}px`,
         top: `${boundingRect.top + scrollY}px`,
         left: `${boundingRect.left + scrollX}px`,
-        'box-shadow': '0 0 0 9999px rgba(0, 0, 0, 0.7)',
-        'z-index': '100'
+        boxShadow: '0 0 0 9999px rgba(0, 0, 0, 0.7)',
+        zIndex: '101'
       };
 
+    this.applyStyles(styles, this.backdropElement);
+  }
+
+  private applyStyles(styles: Partial<CSSStyleDeclaration>, element: HTMLElement) {
     for (const name of Object.keys(styles)) {
-      this.renderer.setStyle(this.backdropElement, name, styles[name]);
+      this.renderer.setStyle(element, name, styles[name]);
     }
   }
+
 }
