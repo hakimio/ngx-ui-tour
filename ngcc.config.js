708f810ed0d8330e82e9d1c62d8122784915c61a
Tomas Rimkus <sandshrew@gmail.com>
2021-08-27 10:35:07 +0200

    Ignore deep imports in "angular2-text-mask" library.

---
+++ ngcc.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    packages: {
+        'angular2-text-mask': {
+            ignorableDeepImportMatchers: [
+                /text-mask-core\//
+            ]
+        }
+    }
+};
