755b02209cd4c79a748d5435bc362fd036a98eaf
Tomas Rimkus <sandshrew@gmail.com>
2021-09-07 13:44:49 +0200

    Updated "md-menu" tour UI default template.

--- projects/ngx-ui-tour-md-menu/src/lib/md-menu.module.ts
+++ projects/ngx-ui-tour-md-menu/src/lib/md-menu.module.ts
@@ -1,37 +1,56 @@
-import { TourAnchorOpenerComponent } from './tour-anchor-opener.component';
-import { CommonModule } from '@angular/common';
-import { ModuleWithProviders, NgModule } from '@angular/core';
-import { MatButtonModule } from '@angular/material/button';
-import { MatCardModule } from '@angular/material/card';
-import { MatIconModule } from '@angular/material/icon';
-import { MatMenuModule } from '@angular/material/menu';
+import {TourAnchorOpenerComponent} from './tour-anchor-opener.component';
+import {CommonModule} from '@angular/common';
+import {ModuleWithProviders, NgModule} from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MatIconModule} from '@angular/material/icon';
+import {MatMenuModule} from '@angular/material/menu';
 
 import {TourBackdropService, TourModule, TourService} from 'ngx-ui-tour-core';
-import { TourAnchorMatMenuDirective } from './tour-anchor.directive';
-import { TourStepTemplateComponent } from './tour-step-template.component';
-import { TourStepTemplateService } from './tour-step-template.service';
+import {TourAnchorMatMenuDirective} from './tour-anchor.directive';
+import {TourStepTemplateService} from './tour-step-template.service';
 import {NgxmTourService} from './ngx-md-menu-tour.service';
+import {TourStepTemplateComponent} from './tour-step-template/tour-step-template.component';
 
-export { TourAnchorMatMenuDirective, TourStepTemplateComponent, NgxmTourService };
+export {
+    TourAnchorMatMenuDirective,
+    TourStepTemplateComponent,
+    NgxmTourService
+};
 
 @NgModule({
-  declarations: [TourAnchorMatMenuDirective, TourStepTemplateComponent, TourAnchorOpenerComponent],
-  entryComponents: [TourAnchorOpenerComponent],
-  exports: [TourAnchorMatMenuDirective, TourStepTemplateComponent, TourModule],
-  imports: [CommonModule, TourModule,
-    MatMenuModule, MatCardModule, MatButtonModule, MatIconModule,
-  ],
+    declarations: [
+        TourAnchorMatMenuDirective,
+        TourStepTemplateComponent,
+        TourAnchorOpenerComponent
+    ],
+    entryComponents: [
+        TourAnchorOpenerComponent
+    ],
+    exports: [
+        TourAnchorMatMenuDirective,
+        TourStepTemplateComponent,
+        TourModule
+    ],
+    imports: [
+        CommonModule,
+        TourModule,
+        MatMenuModule,
+        MatCardModule,
+        MatButtonModule,
+        MatIconModule
+    ]
 })
 export class TourMatMenuModule {
-  public static forRoot(): ModuleWithProviders<TourMatMenuModule> {
-    return {
-      ngModule: TourMatMenuModule,
-      providers: [
-        TourStepTemplateService,
-        TourBackdropService,
-        TourService,
-        NgxmTourService
-      ]
-    };
-  }
+    public static forRoot(): ModuleWithProviders<TourMatMenuModule> {
+        return {
+            ngModule: TourMatMenuModule,
+            providers: [
+                TourStepTemplateService,
+                TourBackdropService,
+                TourService,
+                NgxmTourService
+            ]
+        };
+    }
 }
