596f26cf5fb2d6bd17c0d7eb1603a962bfeeaa48
Tomas Rimkus <sandshrew@gmail.com>
2022-09-02 10:27:17 +0200

    Disable navigation to next step with right arrow key when current step has "nextOnAnchorClick" option enabled.

--- projects/ngx-ui-tour-core/src/lib/tour-hotkey-listener.component.ts
+++ projects/ngx-ui-tour-core/src/lib/tour-hotkey-listener.component.ts
@@ -1,46 +1,52 @@
-import { Component, HostListener } from '@angular/core';
+import {Component, HostListener} from '@angular/core';
+import {TourService, TourState} from './tour.service';
 
-import { TourService, TourState } from './tour.service';
 
 @Component({
-  selector: 'tour-hotkey-listener',
-  template: `<ng-content></ng-content>`
+    selector: 'tour-hotkey-listener',
+    template: `<ng-content></ng-content>`
 })
 export class TourHotkeyListenerComponent {
-  constructor(public tourService: TourService) {}
 
-  /**
-   * Configures hot keys for controlling the tour with the keyboard
-   */
-  @HostListener('window:keydown.Escape')
-  public onEscapeKey(): void {
-    if (
-      this.tourService.getStatus() === TourState.ON &&
-      this.tourService.isHotkeysEnabled()
-    ) {
-      this.tourService.end();
-    }
-  }
+    constructor(
+        public readonly tourService: TourService
+    ) {}
 
-  @HostListener('window:keydown.ArrowRight')
-  public onArrowRightKey(): void {
-    if (
-      this.tourService.getStatus() === TourState.ON &&
-      this.tourService.hasNext(this.tourService.currentStep) &&
-      this.tourService.isHotkeysEnabled()
-    ) {
-      this.tourService.next();
+    /**
+     * Configures hot keys for controlling the tour with the keyboard
+     */
+    @HostListener('window:keydown.Escape')
+    public onEscapeKey(): void {
+        if (
+            this.tourService.getStatus() === TourState.ON &&
+            this.tourService.isHotkeysEnabled()
+        ) {
+            this.tourService.end();
+        }
     }
-  }
 
-  @HostListener('window:keydown.ArrowLeft')
-  public onArrowLeftKey(): void {
-    if (
-      this.tourService.getStatus() === TourState.ON &&
-      this.tourService.hasPrev(this.tourService.currentStep) &&
-      this.tourService.isHotkeysEnabled()
-    ) {
-      this.tourService.prev();
+    @HostListener('window:keydown.ArrowRight')
+    public onArrowRightKey(): void {
+        const step = this.tourService.currentStep;
+
+        if (
+            this.tourService.getStatus() === TourState.ON &&
+            this.tourService.hasNext(this.tourService.currentStep) &&
+            this.tourService.isHotkeysEnabled() &&
+            !step?.nextOnAnchorClick
+        ) {
+            this.tourService.next();
+        }
     }
-  }
+
+    @HostListener('window:keydown.ArrowLeft')
+    public onArrowLeftKey(): void {
+        if (
+            this.tourService.getStatus() === TourState.ON &&
+            this.tourService.hasPrev(this.tourService.currentStep) &&
+            this.tourService.isHotkeysEnabled()
+        ) {
+            this.tourService.prev();
+        }
+    }
 }
