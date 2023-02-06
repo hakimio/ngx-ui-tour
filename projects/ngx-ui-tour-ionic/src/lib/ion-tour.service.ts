9db9b58df4dfeec33d365a1369821ebcfd309ff3
Tomas Rimkus <sandshrew@gmail.com>
2023-02-06 14:05:07 +0100

    Ionic popover fixes: - Fix backdrop on top of tour step - Fix incorrect step position after navigation

--- projects/ngx-ui-tour-ionic/src/lib/ion-tour.service.ts
+++ projects/ngx-ui-tour-ionic/src/lib/ion-tour.service.ts
@@ -1,8 +1,24 @@
 import {IonStepOption} from './step-option.interface';
 import {TourService} from 'ngx-ui-tour-core';
-import {Injectable} from '@angular/core';
+import {inject, Injectable} from '@angular/core';
+import {Config} from '@ionic/angular';
 
 @Injectable({
     providedIn: 'root'
 })
-export class IonTourService<T extends IonStepOption = IonStepOption> extends TourService<T> {}
+export class IonTourService<T extends IonStepOption = IonStepOption> extends TourService<T> {
+
+    private readonly config = inject(Config);
+
+    public initialize(steps: T[], stepDefaults?: T) {
+        const isIOS = this.config.get('mode') === 'ios';
+
+        stepDefaults ??= {} as T;
+        stepDefaults.backdropConfig ??= {};
+        stepDefaults.backdropConfig.parentContainer ??= 'ion-app';
+        stepDefaults.delayAfterNavigation ??= isIOS ? 700: 500;
+
+        super.initialize(steps, stepDefaults);
+    }
+
+}
