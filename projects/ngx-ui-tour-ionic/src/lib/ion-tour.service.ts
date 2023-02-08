be2719184e5a16d24813f068f596c00df8f0d407
Tomas Rimkus <sandshrew@gmail.com>
2023-02-08 11:00:31 +0100

    Fix Ionic tour popover not showing-up when "smoothScroll" is enabled and the page is scrolled to reveal active tour anchor.

--- projects/ngx-ui-tour-ionic/src/lib/ion-tour.service.ts
+++ projects/ngx-ui-tour-ionic/src/lib/ion-tour.service.ts
@@ -2,6 +2,7 @@ import {IonStepOption} from './step-option.interface';
 import {TourService} from 'ngx-ui-tour-core';
 import {inject, Injectable} from '@angular/core';
 import {Config} from '@ionic/angular';
+import {DOCUMENT} from '@angular/common';
 
 @Injectable({
     providedIn: 'root'
@@ -9,14 +10,18 @@ import {Config} from '@ionic/angular';
 export class IonTourService<T extends IonStepOption = IonStepOption> extends TourService<T> {
 
     private readonly config = inject(Config);
+    private readonly document = inject(DOCUMENT);
 
     public initialize(steps: T[], stepDefaults?: T) {
-        const isIOS = this.config.get('mode') === 'ios';
+        const isIOS = this.config.get('mode') === 'ios',
+            ionContent = this.document.documentElement.querySelector('ion-content'),
+            scrollContainer = ionContent?.shadowRoot.querySelector('[part=scroll]') as HTMLElement;
 
         stepDefaults ??= {} as T;
         stepDefaults.backdropConfig ??= {};
         stepDefaults.backdropConfig.parentContainer ??= 'ion-app';
         stepDefaults.delayAfterNavigation ??= isIOS ? 700: 500;
+        stepDefaults.scrollContainer ??= scrollContainer;
 
         super.initialize(steps, stepDefaults);
     }
