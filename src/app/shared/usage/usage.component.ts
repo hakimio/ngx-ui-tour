8926e0a23a64c27ec609b188ae963991703f9c26
Tomas Rimkus <sandshrew@gmail.com>
2023-08-23 13:02:31 +0200

    Added usage note to the documentation app.

--- src/app/shared/usage/usage.component.ts
+++ src/app/shared/usage/usage.component.ts
@@ -1,6 +1,6 @@
-import {Component} from '@angular/core';
+import {Component, Input} from '@angular/core';
 import {default as tourSeviceExample} from '!!raw-loader!./examples/tour-service-initialize.txt';
-import {TuiLinkModule} from '@taiga-ui/core';
+import {TuiLinkModule, TuiNotificationModule} from '@taiga-ui/core';
 import {TuiDocCodeModule} from '@taiga-ui/addon-doc';
 import {ProxyTourAnchorDirective} from '../proxy-tour-anchor.directive';
 import {HeaderComponent} from '../header/header.component';
@@ -13,10 +13,14 @@ import {HeaderComponent} from '../header/header.component';
         HeaderComponent,
         ProxyTourAnchorDirective,
         TuiDocCodeModule,
-        TuiLinkModule
+        TuiLinkModule,
+        TuiNotificationModule
     ]
 })
 export class UsageComponent {
+
+    @Input({required: true})
+    moduleName: string;
 
     readonly examples = {
         tourSeviceExample
