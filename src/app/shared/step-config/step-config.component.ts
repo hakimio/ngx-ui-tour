1afded95e2651263bc2cb5af4af03149c2a90d6e
EliotRagueneau <eragueneau@ebi.ac.uk>
2023-10-13 14:31:12 +0100

    :art: original format code

--- src/app/shared/step-config/step-config.component.ts
+++ src/app/shared/step-config/step-config.component.ts
@@ -7,30 +7,30 @@ import {TuiDocDocumentationModule} from '@taiga-ui/addon-doc';
 import {HeaderComponent} from '../header/header.component';
 
 @Component({
-  selector: 'app-step-config',
-  templateUrl: './step-config.component.html',
-  styleUrls: ['./step-config.component.scss'],
-  standalone: true,
-  imports: [
-    HeaderComponent,
-    TuiDocDocumentationModule,
-    ProxyTourAnchorDirective,
-    NgIf
-  ]
+    selector: 'app-step-config',
+    templateUrl: './step-config.component.html',
+    styleUrls: ['./step-config.component.scss'],
+    standalone: true,
+    imports: [
+        HeaderComponent,
+        TuiDocDocumentationModule,
+        ProxyTourAnchorDirective,
+        NgIf
+    ]
 })
 export class StepConfigComponent {
 
-  @Input()
-  isCloseOnOutsideClickVisible = false;
+    @Input()
+    isCloseOnOutsideClickVisible = false;
 
-  @Input()
-  isShowArrowVisible = false;
+    @Input()
+    isShowArrowVisible = false;
 
-  @Input()
-  isUseLegacyTitleVisible = false;
+    @Input()
+    isUseLegacyTitleVisible = false;
 
-  @ContentChild(PlacementConfigComponent)
-  placementConfig: PlacementConfigComponent;
+    @ContentChild(PlacementConfigComponent)
+    placementConfig: PlacementConfigComponent;
 
-  readonly duplicateAnchorHandlingValues: IStepOption['duplicateAnchorHandling'][] = ['error', 'registerFirst', 'registerLast'];
+    readonly duplicateAnchorHandlingValues: IStepOption['duplicateAnchorHandling'][] = ['error', 'registerFirst', 'registerLast'];
 }
