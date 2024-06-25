f79656b5c06974e30c546c415d755253d2f31d34
Tomas Rimkus <sandshrew@gmail.com>
2024-06-25 10:04:14 +0200

    Remove unused typings.

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -1,18 +1,4 @@
-declare module '*';
-
-declare module '!!file-loader!*' {
-  const result: string;
-
-  export = result;
-}
-
 declare module '!!raw-loader!*' {
-  const result: string;
-
-  export = result;
-}
-
-declare module '*.html' {
   const result: string;
 
   export = result;
